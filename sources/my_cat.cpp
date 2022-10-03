7364cf3ded25da743599b96cee75d2316059a55e
P4r4dis <adil.d.pro@gmail.com>
2022-10-03 20:04:46 +0200

    [UPDATE] coding style updated

--- sources/my_cat.cpp
+++ sources/my_cat.cpp
@@ -5,7 +5,7 @@ unsigned int		check_nb_params(unsigned int ac)
 	if (ac < 2)
 	{
 		std::cerr << "my_cat : Usage : ./my_cat file [...]";
-        return EXIT_FAILURE;
+		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
